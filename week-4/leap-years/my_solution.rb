e04f158d75cf079b46f0cdc5381120bc98a7588c
Alan Alcesto <AlanAlcesto@Alans-MacBook-Pro.local>
2015-10-22 13:32:55 -0500

    Add leap-year dir and files.

---
+++ week-4/leap-years/my_solution.rb
@@ -0,0 +1,16 @@
+# Leap Years
+
+# I worked on this challenge [by myself, with: ].
+
+
+# Your Solution Below
+
+def leap_year?(year)
+	if year % 400 != 0 && year % 100 == 0
+		p false
+	elsif year % 4 == 0
+		p true
+	else
+		p false
+	end
+end
